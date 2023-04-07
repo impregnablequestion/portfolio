a4236a00b5463563bcd3d6cb8f2e4f12ad042c42
Séamus Ryan <ryans40@tcd.ie>
2023-04-07 13:10:22 +0100

    filled out project and updated sketch

--- src/App.js
+++ src/App.js
@@ -4,23 +4,34 @@ import NavBar from './components/NavBar';
 import Home from './components/Home';
 import Background from './Sketch.js';
 import Error from './Error';
-import BellJarRota from './components/Projects/BellJarRota';
+import BellJarText from './components/ProjectText/BellJarText';
+import Project from './components/Project';
+import PPLText from './components/ProjectText/PPLText';
+import SynthedText from './components/ProjectText/SynthedText';
 
 function App() {
 
   return (
     <div className='.App'>
       <Router>
-      <Background/>
-      <NavBar/>
-      <Routes>
-        <Route exact path='/' element={<Home/>}/>
-        <Route path='/portfolio' element={<Home/>}/>
-        <Route path='/about' element={<Home/>}/>
-        <Route path='/bell-jar-rota-project' element={<BellJarRota/>}/>
-        <Route path='/github-repos' element={<Home/>}/>
-        <Route path="*" element={<Error/>}/>
-      </Routes>
+        <Background />
+        <NavBar />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/portfolio' element={<Home />} />
+          <Route path='/about' element={<Home />} />
+          <Route path='/bell-jar-rota-project' element={
+            <Project video="https://vimeo.com/800257110" Text={BellJarText} />
+          } />
+          <Route path='/power-pong-league-project' element={
+            <Project video="https://vimeo.com/800257224" Text={PPLText} />
+          } />
+          <Route path='/synthed-project' element={
+            <Project video="https://vimeo.com/814263635" Text={SynthedText} />
+          } />
+          <Route path='/github-repos' element={<Home />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
       </Router>
     </div>
   );
---
+++ src/components/ProjectText/PPLText.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const PPLText = () => {
+  return (
+    <div>
+      <p>
+        Power Pong League was a group project made with Jill Atkins and
+        James Blaney
+      </p>
+    </div>
+  )
+}
+
+export default PPLText
